f78b90d7677adf66664db984c5f2e60d7e106249
Rup medhi <rupmmedhi@gmail.com>
2025-04-12 00:19:17 +0530

    Adding a Form

--- src/components/CartItems/Item.js
+++ src/components/CartItems/Item.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Item.css'
+import Button from '../UI/Button'
 
 export default function Item({ title, description, price }) {
+  const [count, setCount] = useState(1)
+
+  const handleChange = (e) => {
+    setCount(Number(e.target.value));
+  };
+
   return (
     <div className='meal-main-div'>
       <div>
         <h3>{title}</h3>
         <span className='description'>{description}</span>
-        <span style={{display:"block"}} className='meal-price'>${price}</span>
-       
+        <span style={{ display: "block" }} className='meal-price'>${price}</span>
       </div>
 
-      <div></div>
-   
+      <div className='meal-child-div2'>
+        <div>
+          <h4>Amount</h4>
+          <input
+            type="number"
+            value={count}
+            min={1}
+            onChange={handleChange}
+          />
+        </div>
+
+        <Button>+Add</Button>
+      </div>
+
     </div>
   )
 }
